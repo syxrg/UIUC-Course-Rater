2b1bef632bdfe47170ebeb2b9a073768a7762271
ajsun2 <ajsun2@illinois.edu>
2023-11-30 16:08:13 -0600

    Parsed courses

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+
+import Papa from 'papaparse';
+import './App.css';
 
 function App() {
+  const [csvData, setCsvData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/courses.csv');
+      const reader = response.body.getReader();
+      const result = await reader.read();
+      const text = new TextDecoder('utf-8').decode(result.value);
+
+      Papa.parse(text, {
+        complete: function (result) {
+          // Parsed data in result.data
+          console.log(result.data);
+          setCsvData(result.data);
+        },
+        header: true, 
+      });
+    };
+
+    fetchData();
+  }, []); 
+
   return (
     <div className="App">
       <div class="topnav">
@@ -12,18 +35,16 @@ function App() {
         <a href="#browse" class="active">Browse</a>
       </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h2>CSV Data:</h2>
+          <ul>
+            {csvData.map((row, index) => (
+              <li key={index}>
+                {row.Name}
+              </li>
+            ))}
+          </ul>
+        </div>
       </header>
     </div>
   );
