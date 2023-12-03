b22bb441c674204f182e5a4b40956ab01c2b6ba2
Serena Gong <sxgong2@illinois.edu>
2023-12-03 18:43:47 +1300

    testing

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // serves static files from the React app
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // add API routes here 
 app.get('/api', (req, res) => {
@@ -13,8 +13,9 @@ app.get('/api', (req, res) => {
 
 // sends requests to react if no routes match (keep at very end)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
