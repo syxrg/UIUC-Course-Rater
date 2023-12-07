9ab5aedc8aa88c676a981162ac966ba8be98fb82
Serlina Ku <sku6@illinois.edu>
2023-12-07 16:57:49 -0600

    add info box in browsing

--- frontend/src/components/Class.js
+++ frontend/src/components/Class.js
@@ -17,27 +17,52 @@ const Class = (props) => {
     <div className="App">
       {match ? (
         <>
-          <h1 style={{ textAlign: "left", paddingLeft: "50px" }}>
-            {" "}
-            {match.Subject} {match.Number}: {match.Name}
-            <br />
-            {match.CRN}
-          </h1>
+          <div
+            style={{
+              textAlign: "left",
+              paddingLeft: "50px",
+              paddingRight: "50px",
+            }}
+          >
+            <h1>
+              {" "}
+              {match.Subject} {match.Number}: {match.Name}
+              <br />
+              {match.CRN}
+            </h1>
+            <div
+              style={{
+                fontSize: "36px",
+              }}
+            >
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+              <link
+                rel="stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+              ></link>
+            </div>
+            <div style={{ paddingTop: "10px" }}>
+              Description: {match.Description}
+            </div>
+            <div
+              style={{
+                border: "1px solid #BEBEBE",
+                borderRadius: "10px",
+                height: "100px",
+                margin: "20px",
+              }}
+            >
+              INFO
+            </div>
+          </div>
         </>
       ) : (
         <></>
       )}
-      <div style={{ fontSize: "36px", textAlign: "left", paddingLeft: "50px" }}>
-        <span class="fa fa-star checked"></span>
-        <span class="fa fa-star checked"></span>
-        <span class="fa fa-star checked"></span>
-        <span class="fa fa-star"></span>
-        <span class="fa fa-star"></span>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-        ></link>
-      </div>
     </div>
   );
 };
