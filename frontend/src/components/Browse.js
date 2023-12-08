2f85a04c908c76f917d5011addf4146ed173dcfc
Serena Gong <sxgong2@illinois.edu>
2023-12-08 23:54:37 +1300

    browse working

--- frontend/src/components/Browse.js
+++ frontend/src/components/Browse.js
@@ -26,6 +26,106 @@ const Browse = (props) => {
     }));
   };
 
+
+  const handleSearchChange = (event) => {
+    setQuery(event.target.value.toLowerCase());
+  };
+
+  
+  const sortData = (a, b) => {
+    if (trimmedQuery) {
+      if (a.Subject.toLowerCase().includes(trimmedQuery) && !b.Subject.toLowerCase().includes(trimmedQuery)) return -1;
+      if (!a.Subject.toLowerCase().includes(trimmedQuery) && b.Subject.toLowerCase().includes(trimmedQuery)) return 1;
+
+      if (a.Subject === b.Subject) {
+        return parseInt(a.Number, 10) - parseInt(b.Number, 10);
+      }
+
+      return a.Name.localeCompare(b.Name);
+    }
+    
+    if (a.Subject < b.Subject) return -1;
+    if (a.Subject > b.Subject) return 1;
+    return parseInt(a.Number, 10) - parseInt(b.Number, 10);
+  };
+
+  const trimmedQuery = query.trim().toLowerCase();
+
+  const filteredData = data
+    .filter(
+      (row) =>
+      row.Subject.toLowerCase().includes(trimmedQuery) ||
+      row.Number.toLowerCase().includes(trimmedQuery) ||
+      row.Name.toLowerCase().includes(trimmedQuery)
+    )
+    .sort(sortData);
+
+  return (
+    <div className="outer">
+      <h1>Find a Course</h1>
+      <div className="search-stuff">
+        <TextField
+          placeholder="Search by subject"
+          onChange={handleSearchChange}
+          value={query}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+          style={{ width: 800 }}
+        />
+      </div>
+      <ul style={{ listStyleType: "none", padding: "30px" }}>
+        <Grid 
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+          }}
+        >
+          {filteredData
+            .map((row, index) => (
+              <Grid item xs={3} key={index}>
+                <Link to={`/browse/${row.CRN}`}>
+                  <button className="grid"
+                    style={{
+                      padding: "20px",
+                      height: "100%",
+                      width: "100%",
+                      border: buttonHoverStates[index] ? "1px solid light-gray" : "1px solid gray",
+                      borderRadius: "10px",
+                      background: "none",
+                      boxShadow: buttonHoverStates[index] ? "5px 5px 5px #C9C7C6" : "none",
+                    }}
+                    onMouseEnter={() => handleMouseEnter(index)}
+                    onMouseLeave={() => handleMouseLeave(index)}
+                  >
+                    {row.Subject} {row.Number} <br />
+                    {row.Name}
+                  </button>
+                </Link>
+              </Grid>
+            ))}
+        </Grid>
+      </ul>
+    </div>
+  );
+};
+
+export default Browse;
+
+
+
+
+
+
   // useEffect(() => {
   //   // Fetching CSV data
   //   const fetchCSVData = async () => {
@@ -62,70 +162,3 @@ const Browse = (props) => {
   //     setCsvData(csvData);
   //   }
   // }, [query, csvData]);
-
-  return (
-    <div className="outer">
-      <h1>Find a Course</h1>
-      <div className="search-stuff">
-        <TextField
-          placeholder="Search by subject"
-          onChange={(event) => setQuery(event.target.value)}
-          value={query}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-          // change width of search bar
-          style={{ width: 800 }}
-        />
-      </div>
-      <ul style={{ listStyleType: "none", padding: "30px" }}>
-        <Grid 
-          container
-          spacing={{ xs: 2, md: 3 }}
-          columns={{ xs: 4, sm: 8, md: 12 }}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            textAlign: "center",
-          }}
-        >
-          {data
-            .filter(
-              (row, index, self) =>
-                self.findIndex((r) => r.Name === row.Name) === index
-            )
-            .map((row, index) => (
-              <Grid item xs={3} key={index}>
-                <Link to={`/browse/${row.CRN}`}>
-                  <button className="grid"
-                    style={{
-                      padding: "20px",
-                      height: "100%",
-                      width: "100%",
-                      border: buttonHoverStates[index] ? "1px solid light-gray" : "1px solid gray",
-                      borderRadius: "10px",
-                      background: "none",
-                      boxShadow: buttonHoverStates[index] ? "5px 5px 5px #C9C7C6" : "none",
-                      // background: buttonHoverStates[index] ? "#EBECF0" : "none",
-                    }}
-                    onMouseEnter={() => handleMouseEnter(index)}
-                    onMouseLeave={() => handleMouseLeave(index)}
-                  >
-                    {row.Subject} {row.Number} <br />
-                    {row.Name}
-                  </button>
-                </Link>
-              </Grid>
-            ))}
-        </Grid>
-      </ul>
-    </div>
-  );
-};
-
-export default Browse;
